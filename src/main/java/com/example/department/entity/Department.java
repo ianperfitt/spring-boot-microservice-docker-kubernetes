b259c426a153ddfbd44a5c0b24904e8fe01cadbd
Ian Perfitt <iancp@protonnmail.com>
2024-07-06 21:59:04 -0500

    first commit

---
+++ src/main/java/com/example/department/entity/Department.java
@@ -0,0 +1,22 @@
+package com.example.department.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+}
