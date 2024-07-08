a25fefc002058fb6923b03f54560d78a91a28a96
Ian Perfitt <iancp@protonnmail.com>
2024-07-08 16:47:13 -0500

    Created Spring Cloud gateway microservice.
    1. Updated localhost in restTemplate call to spring-application-name that is used in Eureka server.
    
    2. Added @LoadBalanced annotation to restTemplate for discovering microservice host name from eureka.

--- user-service/src/main/java/com/example/user_service/service/UserService.java
+++ user-service/src/main/java/com/example/user_service/service/UserService.java
@@ -30,7 +30,7 @@ public class UserService {
         ResponseTemplateVO vo = new ResponseTemplateVO();
         User user = userRepository.findByUserId(userId);
 
-        Department department = restTemplate.getForObject("http://localhost:9001/departments/" + userId, Department.class);
+        Department department = restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + userId, Department.class);
 
         vo.setUser(user);
         vo.setDepartment(department);
